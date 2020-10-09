a34d52bb5cfbef6690d109a6a6f84c284b5cbd28
Giacomo Magri <giacomo.magri@dextra-sw.com>
2020-10-09 17:59:44 -0300

    mudança na classe connection

--- src/DatabaseController/Connection.py
+++ src/DatabaseController/Connection.py
@@ -4,7 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Connection(object):
+class SingletonMeta(type):
+
+    _instances = {}
+
+    def __call__(cls):
+        if cls not in cls._instances:
+            instance = super().__call__()
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class Connection(metaclass=SingletonMeta):
     _db = None
 
 
