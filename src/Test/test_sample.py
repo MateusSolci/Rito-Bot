bdc77f5b9eda61d0b5eb74dfc6facdaa5a0ed827
Giacomo Magri <giacomo.magri@dextra-sw.com>
2020-11-03 22:41:52 -0300

    teste para a request generica

--- src/Test/test_sample.py
+++ src/Test/test_sample.py
@@ -1,8 +1,12 @@
 from Database_Controller.connection import Connection
-
+from API_Requests.any_request import make_request
 
 def test_singleton_behaviour():
     c1 = Connection()
     c2 = Connection()
     assert id(c1) == id(c2)
 
+
+def test_generic_request():
+    response = make_request("url")
+    assert response == "*Bad Request*"
